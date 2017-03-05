27d091ef9d1543b72e22ac15c9112b15a84d5b11
Space Invader <amazing.space.invader@gmail.com>
2017-03-05 11:16:21 +0300

    add expandable textarea

---
+++ app/blocks/expandable-textarea/expandable-textarea.js
@@ -0,0 +1,6 @@
+import autoSize from 'autosize';
+
+export default () => {
+  const textarea = document.querySelector('.js-expandable-textarea');
+  autoSize(textarea);
+};
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,16 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+// import svg4everybody from 'svg4everybody';
+// import $ from 'jquery';
+//
+// $(() => {
+// 	svg4everybody();
+// });
 
-$(() => {
-	svg4everybody();
+// import autosize from 'autosize';
+import slider from '../blocks/javascript-level/javascript-level';
+import expandableTextarea from '../blocks/expandable-textarea/expandable-textarea';
+
+window.addEventListener('load', () => {
+	// autosize(document.querySelectorAll('.textarea'));
+	slider();
+	expandableTextarea();
 });
